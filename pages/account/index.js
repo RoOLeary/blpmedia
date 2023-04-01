9df9cee6ef59dd8e633c4d930903161118ff9355
Ronan <roleary81@gmail.com>
2023-04-01 18:24:40 +0200

    wrong way, but let's try

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,15 +1,23 @@
 import Layout from '../../components/layout/Layout'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router' 
 
 export default function Account() {
     
+    const router = useRouter();
     const { data: session } = useSession()
 
-  return (
-    <Layout 
-      metaTitle={'Your Account'}
-    >
-      HI! {session ? session.user.name : 'Do One'}
-    </Layout>
-  )
+    if(session){
+        return (
+            <Layout 
+              metaTitle={'Your Account'}
+            >
+              HI! {session ? session.user.name : 'Do One'}
+            </Layout>
+        )
+    } else {
+        router.push('http://localhost:3000/api/auth/signin')
+    }
+
+    return null;
 }
