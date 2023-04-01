5fab16425122d96fd63832b95d44df81ab91cf66
Ronan <roleary81@gmail.com>
2023-04-01 20:25:53 +0200

    fix account:

--- pages/account/index.js
+++ pages/account/index.js
@@ -33,7 +33,7 @@ export default function Account() {
         return (
             <Layout metaTitle="ACCESS DENIED">
             <section className="bg-gray-50">
-              <div className="max-w-7xl min-h-screen px-12 py-12 mx-auto sm:px-6 flex flex-col sm:pt-16">
+              <div className="max-w-7xl min-h-screen px-6 md:px-12 py-12 mx-auto flex flex-col sm:pt-16">
                 
                 {/* Page not found */}
                 <div className="flex flex-col justify-center lg:w-1/2 xl:w-2/5">
