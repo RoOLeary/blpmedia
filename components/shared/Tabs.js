d115344a8ad8e06c9b61e8d5473708d10167f2cb
Ronan <roleary81@gmail.com>
2023-04-02 12:51:34 +0200

    make some corrections on tabs

--- components/shared/Tabs.js
+++ components/shared/Tabs.js
@@ -21,8 +21,8 @@ const Tabs = ({ tabs }) => {
     //   <div>{tabs[activeTab].content}</div>
     // </div>
     <>
-        <div className="container mx-auto border-b border-gray-200">
-            <div className="flex">
+        <div className="mx-auto border-b border-gray-200">
+            <div className="flex flex-col md:flex-row">
                 {tabs.map((tab, index) => (
                 <button
                     key={index}
@@ -31,14 +31,14 @@ const Tabs = ({ tabs }) => {
                     activeTab === index
                         ? 'border-red-600 text-red-600 bg-gray-100'
                         : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-red-600'
-                    } flex-1 inline-flex items-center px-4 py-2 border-b-2 font-medium transition-all duration-300 ease-in-out`}
+                    } flex-1 inline-flex items-center p-4 border-b-2 font-medium transition-all duration-300 ease-in-out`}
                 >
                     {tab.label}
                 </button>
                 ))}
             </div>
             <div className="mt-2 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-                {tabs[activeTab].content}
+                <h2 className="text-2xl">{tabs[activeTab].content}</h2>
             </div>
         </div>
     </>
