3ebd6b807aea50f64493041948240183fe6e9ebe
Ronan <roleary81@gmail.com>
2023-06-21 22:06:08 +0200

    extend layouts to post

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -1,7 +1,6 @@
 import Layout from '../../../components/layout/Layout'
 import CategoryHeader from '../../../components/headers/CategoryHeader'
 export default async function Page({ params }) {
-    // console.log('page:' , params.category);
     return (
         <Layout>
             <CategoryHeader category={params.category} />
