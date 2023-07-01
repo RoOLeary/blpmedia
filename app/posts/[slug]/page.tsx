042a1727e64f37dfc825df4cdd115b9ef3037751
Ronan <roleary81@gmail.com>
2023-07-01 21:44:58 +0200

    tessst

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,36 +1,9 @@
 
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '../../../components/layout/Layout';
 import Post from '../../../components/posts/Post';
 import NextArticle from '../../../components/posts/NextArticle';
-
-export const CheckMark = () => {
-  return (
-    <svg viewBox="0 0 20 20" width="100px" height="100px" className="center-icon">
-      <g fill="#f00">
-        <rect y={10} width={2} height={2} />
-        <rect x={2} y={12} width={2} height={2} />
-        <rect x={4} y={14} width={2} height={2} />
-        <rect x={6} y={16} width={2} height={2} />
-        <rect x={8} y={14} width={2} height={2} />
-        <rect x={10} y={12} width={2} height={2} />
-        <rect x={12} y={10} width={2} height={2} />
-        <rect x={14} y={8} width={2} height={2} />
-        <rect x={16} y={6} width={2} height={2} />
-        <rect x={18} y={4} width={2} height={2} />
-      </g>
-    </svg>
-  );
-};
-
-export const Close = () => {
-  return (
-    <svg viewBox="0 0 20 20" width="20px" height="20px" className="center-icon">
-      <path d="M0,2.9h2.9V0H0V2.9z M2.9,5.7h2.9V2.9H2.9V5.7z M5.7,8.6h2.9V5.7H5.7V8.6z M8.6,11.4h2.9V8.6H8.6V11.4z M5.7,14.3h2.9v-2.9H5.7V14.3z M2.9,17.1h2.9v-2.9H2.9V17.1z M0,20h2.9v-2.9H0V20z M11.4,14.3h2.9v-2.9h-2.9V14.3z M14.3,17.1h2.9v-2.9h-2.9V17.1zM17.1,20H20v-2.9h-2.9V20z M11.4,8.6h2.9V5.7h-2.9V8.6z M14.3,5.7h2.9V2.9h-2.9V5.7z M17.1,2.9H20V0h-2.9V2.9z" />
-    </svg>
-  );
-};
-
-
 
 
 async function getRemotePost(){
@@ -47,14 +20,66 @@ async function getRemotePost(){
   return remotepost;
 }
 
+const slugify = (str) => {
+  console.log(str.toLowerCase());
+  // str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+  // str = str.toLowerCase(); // convert string to lowercase
+  // str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+  //          .replace(/\s+/g, '-') // replace spaces with hyphens
+  //          .replace(/-+/g, '-'); // remove consecutive hyphens
+  // return str;
+}
 
-const Placeholder = (data) => {
-  
+const RemotePost = ({ post }) => {
+  // console.log(post);
+  const { headline, uri, description_text } = post;
   return(
-    <div className={'p-4'}>
-      <h2 className={"font-bold my-2"}>{data.title}</h2>
-      <p>{data.excerpt}</p>  
+    // <div className={'p-4'}>
+    //   <Link href={`${uri}`}><h2 className={"font-bold my-2"}>{headline}</h2></Link>
+    //   <p>{description_text}</p>  
+    // </div>
+    <article key={uri} className="flex space-x-4 sm:space-x-6 lg:space-x-4 mt-4 mb-4">
+            
+    {/* Image */}
+    <Link
+      href={`posts/${uri}`}
+      className="relative z-10 w-24 h-24 overflow-hidden bg-gray-100 group sm:w-28 sm:h-28 lg:w-20 lg:h-20 xl:w-24 xl:h-24">
+        <Image 
+         className="hidden object-cover w-full h-full lg:block" 
+         src="/images/ads/fashion-ad-portrait.png"
+         alt="Fashion ad portrait"
+          width={200} height={200}
+        />
+      
+
+    </Link>
+    
+    {/* Content */}
+    <div className="w-2/3">
+      <div className="flex flex-col justify-center flex-1 w-full h-full">
+        <div>
+          <Link
+            href={`/${uri}`}
+            className="font-black leading-snug tracking-normal text-gray-900 transition duration-300 ease-in-out text-md hover:underline decoration-2 decoration-gray-800">
+
+            {headline}
+
+          </Link>
+        </div>
+
+        {/* Author */}
+        <div className="flex items-center justify-between mt-2">
+        {description_text}
+
+        
+          
+        </div>
+        
+      </div>
     </div>
+    
+  </article>
+    
 
   )
 }
@@ -72,7 +97,7 @@ export default async function Page({ params }){
 
   let remoteposts = await getRemotePost(); 
   const remoteData = Object.entries(remoteposts).map((post, i) => {
-    const output = Object.entries(post[1]).map((p, i) => <Placeholder title={p[1].headline} content={p[1].body_text} excerpt={p[1].description_text} />)
+    const output = Object.entries(post[1]).map((p, i) => <RemotePost key={i} post={p[1]} />) //title={p[1].headline} content={p[1].body_text} excerpt={p[1].description_text} />)
     return output;
   });
 
