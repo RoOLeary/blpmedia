ed7880e3fa74efc474f607b3f82bd11108463b67
Ronan <roleary81@gmail.com>
2023-07-02 18:12:22 +0200

    hmmmmmmmmmmm

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -1,6 +1,28 @@
 import Layout from './../../../components/layout/Layout'
 import CategoryHeader from './../../../components/headers/CategoryHeader'
+
+
+async function getTechData(){
+    
+
+    const remotepost = await fetch('https://content.api.pressassociation.io/v1/service/paservice:sport/item?sort=firstcreated:desc', {
+        headers: {
+            'Accept': 'application/json',
+            'apikey': '2wmhpxengxmes4d9xfdk4a79'
+        }
+    })
+    .then(response => response.json())
+
+  return remotepost;
+}
+
+
+
 export default async function Page({ params }) {
+
+    const techPosts = await getTechData(); 
+    // console.log(techPosts);
+
     return (
         <Layout>
             <CategoryHeader category={params.category} />
