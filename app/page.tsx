7f9cf469c86deb970e8e3051a06d4ed7731dbef0
Ronan <roleary81@gmail.com>
2023-07-06 00:19:36 +0200

    go again

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,6 @@ export default async function Page() {
                     </div>
                 </div>
             </section>
-
             <Newsletter newsletter={newsletter} />        
         </Layout>
     )
