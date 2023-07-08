7ddb530bf710a2b1b4cb782b8d532d59a213b45a
Ronan <roleary81@gmail.com>
2023-07-08 22:14:11 +0200

    restore to last woeking

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -1,17 +1,5 @@
-import Link from 'next/link';
 import Layout from './../../../components/layout/Layout'
 import CategoryHeader from './../../../components/headers/CategoryHeader'
-import Newsletter from './../../../components/shared/Newsletter'
-import SingleColFeed from './../../../components/shared/SingleColFeed'
-import SidebarAd from './../../../components/sidebar/SidebarAd'
-import SidebarArticles from './../../../components/sidebar/SidebarArticles'
-import SidebarSocialLinks from './../../../components/sidebar/SidebarSocialLinks'
-import BannerArticle from './../../../components/shared/BannerArticle'
-import Pagination from './../../../components/shared/Pagination'
-import { getContentPage } from './../../../libs/getContentPage'
-import { getPostsInCategory, getPopularPostsInCategory } from './../../../libs/getPosts'
-import { getAuthors } from './../../../libs/getAuthors'
-
 
 async function getCats(slug){
     const cats = await fetch(`https://craft-ezhk.frb.io/api/category/${slug}.json`, { next: { revalidate: 10 } });
@@ -25,11 +13,12 @@ async function getCats(slug){
 export default async function Page({ params }) {
     const category = params.category
     const techPosts = await getCats(category); 
-    const postsinCat = techPosts.data[0].entries;
-    const newsletter = await getContentPage('content/shared/newsletter.md');
-    const posts = postsinCat.map((post) => post);
+    console.log(techPosts);
+    // const postsinCat = techPosts.data[0].entries;
+    // const newsletter = await getContentPage('content/shared/newsletter.md');
+    // const posts = postsinCat.map((post) => post);
 
-    console.log(posts);
+    // console.log(posts);
     return (
         <Layout>
             <CategoryHeader category={params.category} />
