bf89139c2ac8c595bdd386a6e24d0bf80179b9d5
Ronan <roleary81@gmail.com>
2023-07-09 09:13:23 +0200

    try again

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -32,7 +32,7 @@ export default async function Page({ params }) {
             <section className="relative max-w-xl px-4 py-12 mx-auto lg:max-w-screen-xl sm:py-16 lg:py-24 sm:px-12 md:max-w-3xl lg:px-8">
                 <div className="w-full grid lg:gap-8 xl:gap-12 lg:grid-cols-3">
                     <div className="col-span-2">
-                       <CategorySingleCol posts={postsinCat} />
+                       {/* <CategorySingleCol posts={postsinCat} /> */}
                     </div>
                     {/* Sidebar */}
                     <div className="w-full mt-12 space-y-8 sm:mt-16 lg:mt-0 lg:col-span-1">
@@ -50,7 +50,7 @@ export default async function Page({ params }) {
                   
                   {/* Articles */}
                   <div className="pb-8 mb-6 border-b-2 border-gray-100 sm:pb-10 sm:mb-10">
-                    <CategorySingleCol posts={postsinCat.slice(7,13)} />
+                    {/* <CategorySingleCol posts={postsinCat.slice(7,13)} /> */}
                   </div>
 {/* 
                   <Pagination /> */}
