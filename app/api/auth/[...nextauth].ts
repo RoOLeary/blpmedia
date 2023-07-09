e2f433c8d61096c2ac1e96f410df6f3669fcbe29
Ronan <roleary81@gmail.com>
2023-07-09 18:32:30 +0200

    add context for auth

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -8,7 +8,27 @@ export const authOptions: AuthOptions = {
       clientSecret: String(process.env.GITHUB_SECRET),
     }),
   ],
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+    theme: {
+        colorScheme: "auto", // "auto" | "dark" | "light"
+        brandColor: "#ffffff", // Hex color code
+        logo: "" // Absolute URL to image
+    },
+    callbacks: {
+        async session({ session, token, user }:any): Promise<any> {
+          session.user.username = session.user.name
+            .split(" ")
+            .join("")
+            .toLocaleLowerCase();
+    
+          session.user.uid = token.sub;
+          return session;
+        },
+        async redirect({ url, baseUrl }) {
+            const redirUrl = 'https://blpmedia.vercel.app/account'
+            return redirUrl;
+        }
+    },
 }
 
 export default NextAuth(authOptions)
