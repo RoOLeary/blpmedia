f47ccd79fcd786638e7c9a202c2433c8e3f0e47a
Ronan <roleary81@gmail.com>
2023-07-09 19:36:35 +0200

    annd puuuush

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,15 @@
+import Layout from './../../components/layout/Layout';
+
 export default function Page() {
     return (
+      <Layout>
         <section className="bg-gray-50">
           <div className="max-w-7xl min-h-screen px-6 py-12 mx-auto flex flex-col sm:pt-16">
               <h2 className="mt-3 text-4xl font-medium tracking-normal text-red-800 uppercase md:tracking-tight lg:leading-tight md:text-5xl">Account Dashboard</h2>
               <p>Hi, how are you today?</p>
             </div>
         </section>
+      </Layout>
   )
 }
 
