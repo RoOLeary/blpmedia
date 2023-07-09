ada984f82cb8013bafa3b8f9dcad1f0ac6f83d11
Ronan <roleary81@gmail.com>
2023-07-09 23:11:36 +0200

    finish up here

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -35,13 +35,13 @@ export default async function Page({ params }) {
             <section className="relative max-w-xl px-4 py-12 mx-auto lg:max-w-screen-xl sm:py-16 lg:py-24 sm:px-12 md:max-w-3xl lg:px-8">
                 <div className="w-full grid lg:gap-8 xl:gap-12 lg:grid-cols-3">
                     <div className="col-span-2">
-                       <CategorySingleCol posts={postsinCat} />
+                       <CategorySingleCol posts={postsinCat.slice(0, 6)} />
                     </div>
                     {/* Sidebar */}
                     <div className="w-full mt-12 space-y-8 sm:mt-16 lg:mt-0 lg:col-span-1">
                         <SidebarAd /> 
                         <br />
-                        <CategorySidebarArticles posts={postsinCat.slice(6,12)} header="Most read" />
+                        <CategorySidebarArticles posts={postsinCat.slice(7,12)} header="Most read" />
                     </div>
                 </div>
             </section>
@@ -55,8 +55,11 @@ export default async function Page({ params }) {
                   {/* Articles */}
                   <div className="pb-8 mb-6 border-b-2 border-gray-100 sm:pb-10 sm:mb-10">
                     <CategorySingleCol posts={postsinCat.slice(12,20)} />
+                    <br />
+                    <br />
+                    <Pagination />
                   </div>
-                  <Pagination />
+                  
                 </section>
               </>
             )}
